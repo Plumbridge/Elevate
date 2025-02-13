e23322176a44735d857118b75aa8ad94eeee4cbd
Plumbridge <78428993+Plumbridge@users.noreply.github.com>
2025-02-13 10:29:14 +0000

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,28 @@
-"use client"
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
+'use client'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+import * as React from 'react'
+import { ThemeProvider as NextThemesProvider, useTheme } from 'next-themes'
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <NextThemesProvider defaultTheme="dark" attribute="class" enableSystem={false}>
+      <ForceDarkMode />
+      {children}
+    </NextThemesProvider>
+  )
 }
 
+// Ensure <html> gets the "dark" class
+function ForceDarkMode() {
+  const { theme } = useTheme()
+  
+  React.useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }, [theme])
+
+  return null
+}
