336c5617214cef730bc734f5f5f18849cb31c906
Plumbridge <78428993+Plumbridge@users.noreply.github.com>
2025-05-15 13:34:14 +0100

    Fixed redirect

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import { Check } from 'lucide-react'
+import { Check } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 
